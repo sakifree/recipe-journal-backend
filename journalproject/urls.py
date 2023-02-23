8a53fe644637183e9f371378dfb3e22bda05fceb
Sakinah Freeman <freemansakinah@gmail.com>
2023-02-22 19:38:51 -0500

    adds routes for recipes

--- journalproject/urls.py
+++ journalproject/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from recipes.views import RecipeViewSet
+
+# create a new router
+router = routers.DefaultRouter()
+# register our viewsets
+router.register(r'recipes', RecipeViewSet)
 
 urlpatterns = [
+    # add all of the router urls
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
