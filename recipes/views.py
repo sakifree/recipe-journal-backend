9ece64b5eaaa98e8a96be7755501ebd9a3ce22fc
Sakinah Freeman <freemansakinah@gmail.com>
2023-02-22 19:34:54 -0500

    creates recipe views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Recipe
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import RecipeSerializer
 
 # Create your views here.
+class RecipeViewSet(viewsets.ModelViewSet):
+    # main query for the index route
+    queryset = Recipe.objects.all()
+    # serializer class for serializing output
+    serializer_class = RecipeSerializer
+    # optional permission class set permission level
+    permission_classes = [permissions.AllowAny]
