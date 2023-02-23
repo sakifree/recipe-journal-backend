2c8dc43af80338792c9e1b9fed1e87f23c30f7f6
Sakinah Freeman <freemansakinah@gmail.com>
2023-02-22 19:19:35 -0500

    creates model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.TextField()
+    directions = models.TextField()
